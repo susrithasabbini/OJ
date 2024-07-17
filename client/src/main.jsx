d9e5033ce1111ee4375684b8e6c7b000b0bdc0b8
susrithasabbini <susrithasabbini@gmail.com>
2024-07-17 22:47:43 +0530

    Code cleaning

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { AppProvider } from "./context.jsx";
-import { Provider } from "react-redux";
-import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <AppProvider>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </AppProvider>
-  </Provider>
+  <AppProvider>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </AppProvider>
 );
